313652a3eb070a5902dc7407dabf1a70cffdb6ce
legend <ahlijin@163.com>
2026-05-31 23:02:22 +0800

    0.9.0 update

--- src/vision/depth_processor/setup.py
+++ src/vision/depth_processor/setup.py
@@ -4,7 +4,7 @@ setup(
     name=package_name, version='0.0.1', packages=[package_name],
     data_files=[('share/ament_index/resource_index/packages', ['resource/' + package_name]), ('share/' + package_name, ['package.xml'])],
     install_requires=['setuptools'], zip_safe=True,
-    maintainer='ahlij', maintainer_email='ahlij@example.com',
+    maintainer='legend', maintainer_email='ahlijin@163.com',
     description='Depth processor for object distance estimation',
     license='Apache-2.0', tests_require=['pytest'],
     entry_points={'console_scripts': ['depth_processor_node = depth_processor.depth_processor_node:main']},
